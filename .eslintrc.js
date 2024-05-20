96928752967674be8e202158db2653619bebd114
CristopherBarrios <35430376+CristopherBarrios@users.noreply.github.com>
2024-05-20 16:51:35 -0600

    11.6

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,49 +1,21 @@
+/* eslint-env node */
 module.exports = {
-  "env": {
-    "browser": true,
-    "es6": true,
-    "jest/globals": true
+  'env': {
+    'node': true,
+    'browser': true,
+    'es2021': true,
   },
-  "extends": [
-    "eslint:recommended",
-    "plugin:react/recommended"
-  ],
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
-    },
-    "ecmaVersion": 2018,
-    "sourceType": "module"
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'ecmaVersion': 12,
+    'sourceType': 'module',
   },
-  "plugins": [
-    "react", "jest"
-  ],
-  "rules": {
-    "indent": [
-      "error",
-        2
-      ],
-    "linebreak-style": [
-      "error",
-      "unix"
-    ],
-    "quotes": [
-      "error",
-      "single"
-    ],
-    "semi": [
-      "error",
-      "never"
-    ],
-    "eqeqeq": "error",
-    "no-trailing-spaces": "error",
-    "object-curly-spacing": [
-      "error", "always"
-    ],
-    "arrow-spacing": [
-      "error", { "before": true, "after": true }
-    ],
-    "no-console": "error",
-    "react/prop-types": 0
-  }
+  'rules': {
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'never'],
+    'no-console': 'off',
+    'indent': ['error', 2],
+    'no-undef': 'error',
+    'no-unused-vars': 'error',
+  },
 }
