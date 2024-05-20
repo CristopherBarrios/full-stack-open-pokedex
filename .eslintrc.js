4f0426b43f1b6bdda3d21d39b9368871b0f2f62c
CristopherBarrios <35430376+CristopherBarrios@users.noreply.github.com>
2024-05-20 17:12:23 -0600

    11.6 v2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,35 @@
 /* eslint-env node */
 module.exports = {
+  parser: 'babel-eslint',
   'env': {
     'node': true,
     'browser': true,
     'es2021': true,
   },
-  'extends': 'eslint:recommended',
+  'extends': ['eslint:recommended',
+  'plugin:react/recommended',],
   'parserOptions': {
+    ecmaFeatures: {
+      jsx: true,
+    },
     'ecmaVersion': 12,
     'sourceType': 'module',
   },
+  plugins: [
+    'react',
+  ],
   'rules': {
+    'react/react-in-jsx-scope': 'off',
     'quotes': ['error', 'single'],
     'semi': ['error', 'never'],
     'no-console': 'off',
     'indent': ['error', 2],
     'no-undef': 'error',
     'no-unused-vars': 'error',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
 }
