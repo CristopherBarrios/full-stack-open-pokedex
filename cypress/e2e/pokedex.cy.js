c9e81e4a9d166833d95cd04a912b90bc924f12a6
CristopherBarrios <35430376+CristopherBarrios@users.noreply.github.com>
2024-05-21 15:15:42 -0600

    11.9 v2

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,15 +1,15 @@
 
 describe('Pokedex', function() {
   it('front page can be opened', function() {
-    cy.visit('http://localhost:8080');
-    cy.contains('ivysaur');
-    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.');
-  });
+    cy.visit('http://localhost:8080')
+    cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
 
   it('can navigate to ivysaur page', function() {
-    cy.visit('http://localhost:8080');
-    cy.contains('ivysaur').click();
-    cy.url().should('include', '/pokemon/ivysaur');
-    cy.contains('chlorophyll');
-  });
-});
+    cy.visit('http://localhost:8080')
+    cy.contains('ivysaur').click()
+    cy.url().should('include', '/pokemon/ivysaur')
+    cy.contains('chlorophyll')
+  })
+})
